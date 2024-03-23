bb71f433a6da506703c8ca61db076b006755aff9
geckeys <108212521+geckeys@users.noreply.github.com>
2024-03-23 10:37:45 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,11 +2,11 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = ['+2347017075654']
+global.ownername = "Geckeys"//owner name
+global.ytname = "YT: GeckeysBotz.inc"
+global.socialm = "GitHub: geckeys"
+global.location = "Nigerian"
 
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
